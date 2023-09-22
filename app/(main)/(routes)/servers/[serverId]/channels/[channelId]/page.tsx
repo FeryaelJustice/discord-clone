73518b7f1917169e5c07a8f87c4bef4425723a46
FeryaelJustice <nano9gs@hotmail.es>
2023-09-22 21:21:45 +0200

    send messages socket in a channel

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -47,7 +47,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             <ChatInput
                 name={channel.name}
                 type="channel"
-                apiUrl="api/socket/messages"
+                apiUrl="/api/socket/messages"
                 query={{
                     serverId: channel.serverId,
                     channelId: channel.id
